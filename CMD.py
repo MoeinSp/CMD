ce9b1a8b3077340eae1de075c1a67815258f4151
Moien <moien@example.com>
2025-12-11 12:47:33 +0330

    revolve and mkdir

--- CMD.py
+++ CMD.py
@@ -1,1 +1,35 @@
-s
+from datetime import datetime
+class Node:
+    def __init__(self , name = None , type = None):
+        self.time = datetime.now()
+        self.owner = "user"
+        self.group = "group"
+        self.perms = "755"
+        print("Enter Type")
+        if type is None:
+            print("Enter Type")
+            self.type = input()
+        if name is None:
+            print("Enter Name")
+            self.name = input()
+        if self.type == "file":
+            print("Enter your content.")
+            self.Content = input()
+            print("Enter Size :")
+            self.Size = input()
+        if self.type == "directory":
+            self.children = []
+        self.parent = None
+
+        def resolve_path():
+
+        def mkdir (self ,):
+
+hoom = Node("home","directory")
+root = []
+root.append(hoom)
+root.append(Node())
+current_addres = hoom
+while True:
+    order = input().split()
+    if order[0] == "mkdir":
